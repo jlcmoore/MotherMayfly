44abe218d0dfd4f29bbf113ec5bf8442152913bf
Jared Moore <jlcmoore@users.noreply.github.com>
2018-11-17 17:55:40 -0500

    gifs and more website

--- docs/img/mov_to_gif.sh
+++ docs/img/mov_to_gif.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 mkdir frames
 # crop=out_w:out_h:x:y for video 
-ffmpeg -i $1 -vf crop=$2:$3:$4:$5 -r 4 'frames/frame-%03d.jpg'
-echo "here"
-convert -delay 20 -loop 0 frames/*.jpg $1.gif
+ffmpeg -i $1 -vf crop=$2:$3:$4:$5 -r 12 'frames/frame-%03d.jpg'
+convert -delay $6 -loop 0 frames/*.jpg $1.gif
 rm -rf frames
