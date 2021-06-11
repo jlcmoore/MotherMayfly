637d3814c89edc0bc3f6fc71ad4518bde69f8b5e
Jared Moore <jlcmoore@users.noreply.github.com>
2021-06-11 12:34:54 -0400

    remove topic

--- interval.py
+++ interval.py
@@ -3,20 +3,22 @@ import threading
 import gmail_util
 
 class IntervalThread(threading.Thread):
-    def __init__(self, printer, printer_lock, output_topics):
+    def __init__(self, printer, printer_lock):
         threading.Thread.__init__(self)
         self.printer = printer
         self.printer_lock = printer_lock
-        self.output_topics = output_topics
+        #self.output_poems = output_poems
 
     def run(self):
         emails = gmail_util.get_new_messages()
         messages = []
         if emails:
             for email in emails:
-                if email['Subject'].lower() == 'topic':
-                    topic = email['Message_body'].rstrip('\r\n').lower()
-                    self.output_topics.put(topic)
+                if email['Subject'].lower() == 'poem':
+                    print("Not Implemented")
+                    # will need to call: write_poem(poem, poet_id, cursor):
+                    #topic = email['Message_body'].rstrip('\r\n').lower()
+                    #self.output_poems.put(topic)
                 if email['Subject'].lower() == 'message':
                     messages.append(email)
         for message in messages:
